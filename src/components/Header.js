c67c77224006fef26b8c8c77567736d25956819f
mouhoub bouhoum <mouhoub@mouhoubs-MacBook-Air.local>
2019-04-17 10:05:49 -0500

    clicky game with react

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends Component{
     return (
         <header className="app-header">
             <div className="container">
-                <a className='link' href='/'><h2>Clicky Game</h2></a>
+                <a className='link' href='/clickygame/'><h2>Clicky Game</h2></a>
                 <h2>{this.props.guess}</h2>
                 <h2>score = {this.props.score} | high score = {this.props.highscore}</h2>
             </div>
